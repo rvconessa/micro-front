7446d1b0305b6d068d88063123cddaac3d5a97e0
Renan <rvconessa@gmail.com>
2023-11-05 16:39:57 -0300

    feat: add production URL

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,15 +6,16 @@ const packageJson = require('../package.json');
 const prodConfig = {
     mode: "production",
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'marketing',
-            exposes: {
-				'./MarketingApp': './src/bootstrap'
-			},
-            shared: packageJson.dependencies
+          name: 'marketing',
+          exposes: {
+            './MarketingApp': './src/bootstrap'
+          },
+          shared: packageJson.dependencies
         })
     ]
 }
