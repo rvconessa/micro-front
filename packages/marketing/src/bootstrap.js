82e17993b3895441785c17a5a136a0618f17569f
Renan <rvconessa@gmail.com>
2023-11-14 21:53:03 -0300

    feat: add routes

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 
-const mount = (el) => {
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if(onNavigate) {
+    history.listen(onNavigate)
+  }
+
 	ReactDOM.render(
-		<App />,
+		<App history={history} />,
 		el
 	)
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location
+      if (pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    }
+  }
 }
 
 if(process.env.NODE_ENV === 'development') {
 	const devRoot = document.querySelector('#app_marketing');
 
 	if(devRoot) {
-		mount(devRoot)
+		mount(devRoot, { defaultHistory: createBrowserHistory() })
 	}
 }
 
